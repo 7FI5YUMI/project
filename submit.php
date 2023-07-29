f86a9c0f64358077d9c179ae7f4921224c62c4da
ohamshakya <ohamshakya40@gmail.com>
2023-07-29 18:51:42 +0545

    updated

--- submit.php
+++ submit.php
@@ -5,43 +5,72 @@ require("./database/databaseconn.php");
 
 if (isset($_GET['param1'])&&($_GET['param2'])) {
     // if (isset($_GET['param2'])) {
-       echo  $ticketId = $_GET['param1'];
-        echo $userId = $_GET['param2'];
+       $ticketId = $_GET['param1'];
+         $userId = $_GET['param2'];
     // }
+    if (isset($_POST['stripeToken'])) {
+        \Stripe\Stripe::setVerifySslCerts(false);
+        $token = $_POST['stripeToken'];
+        $data = \Stripe\Charge::create(
+            array(
+                "amount" => 100,
+                "currency" => "usd",
+                "description" => "vehicle parking",
+                "source" => $token,
+            )
+        );
+    
+        $insertAmount = $data->amount;
+        $insertCurrency = $data->currency;
+        // $paymentType = "card";
+        // $status = $data->status;
+    
+    
+        $paymentDate = date('Y-m-d H:i:s');
+    
+        $sql = "INSERT INTO payment(payment_type,payment_date,amount,ticket_id,user_id,currency)VALUES('card','$paymentDate',$insertAmount,$ticketId,$userId,'$insertCurrency')";
+        $result = mysqli_query($conn, $sql);
+        if ($result) {
+            echo '<script>alert("payment successfull")</script>';
+        } else {
+            echo "fail";
+        }
+    }
+    
 }
 // $cardNumber = $_POST['cardnumber'];
 $success = "";
 // \Stripe\Stripe::setVerifySslCerts(false);
 // $token = $_POST['stripeToken'];
 
-if (isset($_POST['stripeToken'])) {
-    \Stripe\Stripe::setVerifySslCerts(false);
-    $token = $_POST['stripeToken'];
-    $data = \Stripe\Charge::create(
-        array(
-            "amount" => 100,
-            "currency" => "usd",
-            "description" => "vehicle parking",
-            "source" => $token,
-        )
-    );
+// if (isset($_POST['stripeToken'])) {
+//     \Stripe\Stripe::setVerifySslCerts(false);
+//     $token = $_POST['stripeToken'];
+//     $data = \Stripe\Charge::create(
+//         array(
+//             "amount" => 100,
+//             "currency" => "usd",
+//             "description" => "vehicle parking",
+//             "source" => $token,
+//         )
+//     );
 
-    $insertAmount = $data->amount;
-    $insertCurrency = $data->currency;
-    // $paymentType = "card";
-    // $status = $data->status;
+//     $insertAmount = $data->amount;
+//     $insertCurrency = $data->currency;
+//     // $paymentType = "card";
+//     // $status = $data->status;
 
 
-    $paymentDate = date('Y-m-d H:i:s');
+//     $paymentDate = date('Y-m-d H:i:s');
 
-    $sql = "INSERT INTO payment(payment_type,payment_date,amount,ticket_id,user_id,currency)VALUES('card','$paymentDate',$insertAmount,$ticketId,$userId,'$insertCurrency')";
-    $result = mysqli_query($conn, $sql);
-    if ($result) {
-        echo '<script>alert("payment successfull")</script>';
-    } else {
-        echo "fail";
-    }
-}
+//     $sql = "INSERT INTO payment(payment_type,payment_date,amount,ticket_id,user_id,currency)VALUES('card','$paymentDate',$insertAmount,$ticketId,$userId,'$insertCurrency')";
+//     $result = mysqli_query($conn, $sql);
+//     if ($result) {
+//         echo '<script>alert("payment successfull")</script>';
+//     } else {
+//         echo "fail";
+//     }
+// }
 
 
 ?>
